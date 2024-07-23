480e0efa84d751d29f080bb057e05ca397e8f92f
Sourav De <72644559+Srv8@users.noreply.github.com>
2024-07-23 15:46:23 +0530

    Update setup.sh
    Will not start the service as root anymore.
    
    The new user will be created.
    
    After node setup backup option provided

--- setup.sh
+++ setup.sh
@@ -2,10 +2,30 @@
 
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root. Use sudo ./setup_juneogo.sh"
+    echo "This script must be run as root. Use sudo ./setup_juneogo.sh (juneo - new user will be created)"
     exit 1
 fi
 
+# Prompt the user for a password or use the default root password
+read -p "Enter a password for the 'juneo' user (leave blank to use root password): " juneo_password
+if [ -z "$juneo_password" ]; then
+    juneo_password=$(grep "^root:" /etc/shadow | cut -d: -f2)
+    echo "Using root password for 'juneo' user."
+else
+    echo "Using provided password for 'juneo' user."
+fi
+
+# Create a new user called juneo if it doesn't exist
+if ! id -u juneo > /dev/null 2>&1; then
+    echo "Creating new user 'juneo'..."
+    useradd -m -s /bin/bash juneo
+    echo "juneo:$juneo_password" | chpasswd
+else
+    echo "User 'juneo' already exists."
+fi
+
+# Switch to the new user and execute the rest of the script
+sudo -u juneo -i <<'EOF'
 # Change to the user's home directory
 cd ~
 
@@ -29,6 +49,8 @@ mkdir -p ~/.juneogo/plugins
 mv ~/juneogo-binaries/plugins/jevm ~/.juneogo/plugins
 mv ~/juneogo-binaries/plugins/srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e ~/.juneogo/plugins
 
+EOF
+
 # Create the systemd service file
 echo "Creating the systemd service file..."
 cat <<EOF | sudo tee /etc/systemd/system/juneogo.service
@@ -38,10 +60,10 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/root/juneogo
-WorkingDirectory=/root
+ExecStart=/home/juneo/juneogo
+WorkingDirectory=/home/juneo
 Restart=on-failure
-User=root
+User=juneo
 
 [Install]
 WantedBy=multi-user.target
@@ -58,5 +80,25 @@ sudo systemctl enable juneogo.service
 # Start the service immediately
 echo "Starting the Juneogo service..."
 sudo systemctl start juneogo.service
+
+# Prompt the user to backup files
+read -p "Would you like to backup the staking and signer files? (yes/no): " backup_choice
+if [ "$backup_choice" == "yes" ]; then
+    echo "Stopping the Juneogo service..."
+    sudo systemctl stop juneogo.service
+
+    # Create the backup directory and backup the files
+    backup_dir="/home/juneo/backup"
+    echo "Creating backup directory at $backup_dir..."
+    mkdir -p $backup_dir
+
+    echo "Backing up staker.crt, staker.key, and signer.key..."
+    cp /home/.juneogo/staking/staker.crt $backup_dir
+    cp /home/.juneogo/staking/staker.key $backup_dir
+    cp /home/.juneogo/staking/signer.key $backup_dir
+
+    echo "Starting the Juneogo service..."
+    sudo systemctl start juneogo.service
+fi
 
 echo "Juneogo setup complete and service started."
