091e0bbb27015070262456a58cdd8ae0882ba533
Sourav De <72644559+Srv8@users.noreply.github.com>
2024-09-13 13:07:06 +0530

    Update mainnet.sh
    patch unzip

--- mainnet.sh
+++ mainnet.sh
@@ -100,11 +100,11 @@ mv /home/juneo/.juneogo/db /home/juneo/.juneogo/db_old
 
 # Download the snapshot DB zip file
 echo "Downloading the snapshot DB file..."
-wget http://212.90.121.86:6969/juneogo_mdb_backup.zip
+wget /home/juneo/.juneogo http://212.90.121.86:6969/juneogo_mdb_backup.zip
 
 # Unzip the snapshot into the .juneogo directory
 echo "Unzipping the snapshot DB file..."
-unzip -o juneogo_mdb_backup.zip -d /home/juneo/.juneogo/
+unzip -o /home/juneo/.juneogo/juneogo_mdb_backup.zip
 
 sleep 10
 
